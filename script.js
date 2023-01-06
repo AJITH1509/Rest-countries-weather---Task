79a2f96bd1d0eff8e31fa4d14c4dca62c198f466
AJITH1509 <ajithmuthu799@gmail.com>
2023-01-06 21:26:31 +0530

    updated stylings

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-var url = "https://restcountries.com/v3.1/all";
-var bodyy = document.querySelector(".row");
+let url = "https://restcountries.com/v3.1/all";
+let bodyy = document.querySelector(".row");
 
 async function restapi() {
   try {
-    var rest = await fetch(url);
+    let rest = await fetch(url);
     var response = await rest.json();
-    console.log(response);
-    for (var i = 0; i < response.length; i++) {
+    for (let i = 0; i < response.length; i++) {
       var create_div = document.createElement("div");
-      create_div.classList.add("col-md-6");
+      create_div.classList.add("col-md-4");
       create_div.classList.add("col-lg-4");
-      create_div.classList.add("col-sm-12");
+      create_div.classList.add("col-sm-6");
+      create_div.classList.add("col-xl-4");
       create_div.innerHTML = `
       <div class="card-deck">
-      <div class="card border-info">
+      <div class="card h-100">
       <div class="card-header bg-danger" id="header">
-         <h4>${response[i].name.common}</h4>
+         <H1 id="title" class="text-center">${response[i].name.common}</H1>
       </div>
-        <div class="card-body" >
-          <img class="img" src="${response[i].flags.png}">
-         <p class="card-text text-center"> country code: ${response[i].cca3}</p>
-         <p class="card-text text-center">Region: ${response[i].region}</p>
-         <p class="card-text text-center">capital: ${response[i].capital?.length > 0? response[i].capital[0]: "NA"}</p>
-         <p class="card-text text-center">lat lng: ${response[i].latlng[0]},${response[i].latlng[1]}</p>
-         <div class="reports" id="index-${i}"></div>
+      <div class="card-body" >
+           <div class="card-text">
+              <img class="img card-img-top" src="${response[i].flags.png}">
+              <p text-center"> country code: ${response[i].cca3}</p>
+              <p text-center">Region: ${response[i].region}</p>
+              <p text-center">capital: ${response[i].capital?.length > 0? response[i].capital[0]: "NA"}</p>
+              <p text-center">lat lng: ${response[i].latlng[0]},${response[i].latlng[1]}</p>
+              <div class="reports" id="index-${i}"></div>
+            </div>
      </div> 
      <div class="card-footer" >
      <button class="btn btn-primary text-center" onclick="weather('${response[i].name.common}','index-${i}')">Click for Weather</button>
@@ -44,7 +46,6 @@ async function weather(countryname, id) {
   var url2 = `https://api.openweathermap.org/data/2.5/weather?q=${countryname}&appid=44f831ed100824c8de74ae8c5e6b38e5`;
   var weath = await fetch(url2);
   var res = await weath.json();
-  console.log(res);
   var bodyElement = document.getElementById(id);
   var value = (`Temperature: ${res.main.temp} Sky:${res.weather[0].main}`);
   bodyElement.append(value);
